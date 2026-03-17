026d1e82fb3ad604f24c4058f4915a18ca55e7f1
Akash Vijayan <akash.it2027@gmail.com>
2026-03-17 14:51:41 +0530

    Backed Completed

--- app/server.js
+++ app/server.js
@@ -1,10 +1,21 @@
+require("dotenv").config();
 const express = require("express");
+const connectDB = require("./config/db");
+
 const app = express();
 
+// Connect DB
+connectDB();
+
+app.use(express.json());
+
+// Test route
 app.get("/", (req, res) => {
-  res.send("Hospital Management System Running 🚀");
+  res.send("Hospital API Running 🚀");
 });
 
-app.listen(3000, () => {
-  console.log("Server started on port 3000");
+app.listen(3000, "0.0.0.0", () => {
+  console.log("Server running on port 3000");
 });
+app.use(express.json());
+app.use("/patients", require("./routes/patientRoutes"));
