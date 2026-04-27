d5eb2c8716c5eb1f216e5512c5eaf464b591e028
Akash Vijayan <akash.it2027@gmail.com>
2026-04-27 18:47:16 +0530

    Frontend

--- app/server.js
+++ app/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+<<<<<<< HEAD
 
 const app = express();   // ✅ FIRST create app
 
@@ -20,9 +21,28 @@ app.get("/", (req, res) => {
   res.send("Hospital API is running 🚀");
 });
 
+=======
+
+const app = express();   // ✅ create app FIRST
+
+app.use(cors());         // ✅ then use middleware
+app.use(express.json());
+
+// routes
+const authRoutes = require("./routes/authRoutes");
+const patientRoutes = require("./routes/patientRoutes");
+
+app.use("/auth", authRoutes);
+app.use("/patients", patientRoutes);
+
+>>>>>>> 1fc472b (ui)
 // start server
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+<<<<<<< HEAD
 });
+=======
+});
+>>>>>>> 1fc472b (ui)
